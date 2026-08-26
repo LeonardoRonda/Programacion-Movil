14f1793590ad8f750032d7f26b5efdaab0228779
Leonardo Favio Ronda Vidalon <leonardo.ronda@tecsup.edu.pe>
2026-08-26 00:37:04 -0500

    Delete semana2/Lab02CarritoKotlin directory

--- semana2/Lab02CarritoKotlin/app/src/main/java/com/ronda/lab2carritokotlin/Carrito.kt
+++
@@ -1,7 +0,0 @@
-package com.ronda.lab2carritokotlin
-
-fun main() {
-    println("=========================================")
-    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
-    println("=========================================")
-}
