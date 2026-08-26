2033018af27501165eb156d88df2759c251a552f
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-08-26 00:27:50 -0500

    “Proyecto inicial con menú del carrito”.

---
+++ semana2/Lab02CarritoKotlin/app/src/main/java/com/ronda/lab2carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.ronda.lab2carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
